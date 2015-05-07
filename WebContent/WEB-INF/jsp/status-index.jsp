c96a6b71ce18583c8a1c20c6cf2596628bfe9dd2
Austin McKinley <austin.mckinley@acumendigital.com>
2015-05-06 20:54:35 -0600

    Added formatting for turn representation. Fixed target and source build configurations to use Java 8.

--- WebContent/WEB-INF/jsp/status-index.jsp
+++ WebContent/WEB-INF/jsp/status-index.jsp
@@ -1,12 +1,31 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<jsp:useBean id="playerTurnStatuses" scope= "request" type= "java.util.List<austinmckinley.pitboss.status.dataobjects.PlayerTurnStatus>"/>
 <html>
 <head>
-<title>Test</title>
+<title>Status</title>
 
 </head>
 <body>
 	<br>
 	<div style="text-align: center">
-		<h1>${statusRecord}</h1>
+        <h1>Player Statuses</h1>
+	    <table>
+            <c:forEach items="${playerTurnStatuses}" var="playerTurnStatus">
+                <tr>
+                    <td>${playerTurnStatus.getPlayerName()}</td>
+                    <td>
+                        <c:choose>
+                            <c:when test="${playerTurnStatus.isTurnFinished()}">
+                                Turn Finished
+                            </c:when>
+                            <c:otherwise>
+                                Turn Pending
+                            </c:otherwise>
+                        </c:choose>
+                    </td>
+                </tr>
+            </c:forEach>
+        </table>
 	</div>
 </body>
 </html>
--- src/austinmckinley/pitboss/status/StatusController.java
+++ src/austinmckinley/pitboss/status/StatusController.java
@@ -1,10 +1,15 @@
 package austinmckinley.pitboss.status;
 
+import austinmckinley.pitboss.status.dataobjects.PlayerTurnStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Controller
 @RequestMapping("/status")
@@ -14,7 +19,14 @@ public class StatusController {
 
 	@RequestMapping(value="/test", method = RequestMethod.GET)
 	public String getIndex(Model model) {
-		model.addAttribute("statusRecord", statusParser.getCurrentStatus().toString());
+        List<PlayerTurnStatus> playerTurnStatuses = statusParser
+                .getCurrentStatus()
+                .getPlayerStatuses()
+                .entrySet()
+                .stream()
+                .map(entry -> entry.getValue())
+                .collect(toList());
+		model.addAttribute("playerTurnStatuses", playerTurnStatuses);
 		return "status-index";
 	}
 }
